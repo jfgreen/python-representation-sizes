e2a4a2bccd61bc13bd6dff3fe2cd3abddf12d493
James Green <1288679+jfgreen@users.noreply.github.com>
2020-03-02 09:18:22 +0000

    Print benchmark before stats

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -5,4 +5,4 @@ fruit_count=1000000
 
 benchmark_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-find $benchmark_dir -name "*.py" -exec /usr/bin/time -l {} $fruit_count \; -print
+find $benchmark_dir -name "*.py" -print -exec /usr/bin/time -l {} $fruit_count \;
